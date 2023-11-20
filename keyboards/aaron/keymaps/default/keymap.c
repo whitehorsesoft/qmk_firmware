17aeb2cd99d249c6cbc389ab4ac7e230f9b038f6
aaron <devops@whitehorsesoft.com>
2023-11-20 10:52:45 -0600

    added first layer

--- keyboards/aaron/keymaps/default/keymap.c
+++ keyboards/aaron/keymaps/default/keymap.c
@@ -8,8 +8,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_QUOTE, KC_COMMA, KC_DOT, KC_P, KC_Y,                                KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_NO,
         KC_CAPS_LOCK, KC_A,   ALT_T(KC_O),  CTL_T(KC_E),  SFT_T(KC_U),   LT(4, KC_I),                                LT(4, KC_D), SFT_T(KC_H), CTL_T(KC_T), ALT_T(KC_N), KC_S, KC_NO,
         KC_NO, KC_GRAVE,    LT(3, KC_Q),  LT(2, KC_J), LT(1, KC_K), KC_X                                KC_B,    KC_M,    KC_W, KC_V,  KC_Z, KC_NO,
-        LGUI(KC_DOWN), LGUI(KC_UP), LGUI(KC_TAB), KC_ENT, KC_TAB, KC_DEL,           KC_BSPC,  KC_SPC,  KC_B, KC_LBRC,  KC_RBRC, KC_LEFT, KC_RIGHT,
-        KC_HOME, KC_END,    KC_ESC,           KC_GRAVE,  KC_CAPS,  KC_APP
+        LGUI(KC_DOWN), LGUI(KC_UP), LGUI(KC_TAB), KC_LGUI, KC_ENT, KC_TAB, LCTL(KC_A),           LCTL(KC_BSPC),  KC_BSPC,  KC_SPC, KC_NO,  KC_NO, KC_NO, RGB_TOG,
+        LCTL(KC_C),    LCS(KC_C), KC_NO, KC_DEL, LCS(KC_V),  LCTL(KC_V)
         )
 //[0] = LAYOUT_manuform(
 //    KC_NO, KC_QUOTE, KC_COMMA, KC_DOT, KC_P, KC_Y,                                KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_NO,
